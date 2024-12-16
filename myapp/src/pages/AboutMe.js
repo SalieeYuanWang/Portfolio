3a3276dab41ec1519e0cc7872b193a2542f78b68
Yuan Wang <sandiwking@gmai.com>
2024-12-16 10:48:52 -0500

    changed size of the project logo and resume link

--- myapp/src/pages/AboutMe.js
+++ myapp/src/pages/AboutMe.js
@@ -6,8 +6,8 @@ function AboutMe() {
     <div className="about">
       <h1>About Me</h1>
       <img src="/profilepic.jpg" alt="profile" className="profile-pic" />
-      <p>My name is Sally Wang, I'm currently a student in the Software Engineering Technician program at Centennial College. I am from China.</p>
-      <a href="/Sally Wang CV.pdf" target="_blank" rel="noopener noreferrer">View My Resume</a>
+      <p>My name is Sally Wang, I'm currently a student in Software Engineering Technician program at Centennial College.</p>
+      <a href="\Sally_Yuan_Wang_CV.pdf" target="_blank" rel="noopener noreferrer">View My Resume</a>
     </div>
   );
 }
--- myapp/src/pages/Projects.js
+++ myapp/src/pages/Projects.js
@@ -5,15 +5,15 @@ function Projects() {
     <div className="projects">
       <h1>My Projects</h1>
       <div className="project-item">
-        <img src="/image1.png" alt="Project 1" />
+        <img src="/image1.png" alt="Project 1" style={{ width: '150px', height: '150px', objectFit: 'cover' }}/>
         <p>Project 1 description goes here.</p>
       </div>
       <div className="project-item">
-        <img src="/image2.png" alt="Project 2" />
+        <img src="/image2.png" alt="Project 2" style={{ width: '150px', height: '150px', objectFit: 'cover' }}/>
         <p>Project 2 description goes here.</p>
       </div>
       <div className="project-item">
-        <img src="/image3.jpg" alt="Project 3" />
+        <img src="/image3.jpg" alt="Project 3" style={{ width: '150px', height: '150px', objectFit: 'cover' }}/>
         <p>Project 3 description goes here.</p>
       </div>
     </div>
